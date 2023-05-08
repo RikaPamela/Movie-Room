d4c2d5aa2ee6c3f5357fac599c70934bae414d19
Karabo Modika <80250498+enikma-mod@users.noreply.github.com>
2023-05-08 16:32:20 +0200

    update

--- Backend/app/models/moviesDB.js
+++ Backend/app/models/moviesDB.js
@@ -1,54 +1,26 @@
-const mongoose = require('mongoose');
+// // models/Movie.js
 
-const movieSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  releaseYear: {
-    type: Number,
-    required: true
-  },
-  director: {
-    type: String,
-    required: true
-  },
-  genre: {
-    type: String,
-    required: true
-  },
-  rating: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 10
-  },
-  cast: {
-    type: [String],
-    required: true
-  },
-  plot: {
-    type: String,
-    required: true
-  },
-  poster: {
-    type: String,
-    required: true
-  },
-  trailer: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+// const mongoose = require('mongoose');
 
-const Movie = mongoose.model('Movie', movieSchema);
+// const movieSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   description: {
+//     type: String,
+//     required: true
+//   },
+//   ratings: {
+//     type: Number,
+//     required: true
+//   },
+//   image: {
+//     type: String,
+//     required: true
+//   }
+// });
 
-module.exports = Movie;
+// const Movie = mongoose.model('Movie', movieSchema);
+
+// module.exports = Movie;
